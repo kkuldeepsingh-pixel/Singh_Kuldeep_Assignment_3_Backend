6df60b97bd66eaa12488316196e95653621c19dd
Kuldeep Singh <kkuldeepsingh@academic.rrc.ca>
2026-02-20 15:29:23 -0600

    feat: added optional fields with default values

--- src/api/v1/validation/eventValidation.ts
+++ src/api/v1/validation/eventValidation.ts
@@ -4,6 +4,10 @@ export const createEventSchema = Joi.object({
     name: Joi.string().min(3).required(),
     date: Joi.date().iso().required(),
     capacity: Joi.number().integer().min(5).required(),
+    registrationCount: Joi.number().integer().default(0),
+    status: Joi.string()
+        .valid('active', 'cancelled', 'completed')
+        .default('active'),
     category: Joi.string()
     .valid('conference', 'workshop', 'meetup', 'seminar', 'general')
     .required(),
