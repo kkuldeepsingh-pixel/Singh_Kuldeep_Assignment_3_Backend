0e2365b5810954c1c9d0a9480fde62858aee3016
Kuldeep Singh <kkuldeepsingh@academic.rrc.ca>
2026-02-20 15:24:19 -0600

    feat: added min integer validation for capacity

--- src/api/v1/validation/eventValidation.ts
+++ src/api/v1/validation/eventValidation.ts
@@ -3,7 +3,7 @@ import Joi from 'joi';
 export const createEventSchema = Joi.object({
     name: Joi.string().min(3).required(),
     date: Joi.date().iso().required(),
-    capacity: Joi.number().integer().required(),
+    capacity: Joi.number().integer().min(5).required(),
     category: Joi.string()
     .valid('conference', 'workshop', 'meetup', 'seminar', 'general')
     .required(),
