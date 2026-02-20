dfdcb062caa050a477a07379a4690fe78e49a62b
Kuldeep Singh <kkuldeepsingh@academic.rrc.ca>
2026-02-20 15:20:21 -0600

    fix: remove status field from createEventSchema

--- src/api/v1/validation/eventValidation.ts
+++ src/api/v1/validation/eventValidation.ts
@@ -3,7 +3,6 @@ import Joi from 'joi';
 export const createEventSchema = Joi.object({
     name: Joi.string().required(),
     date: Joi.date().iso().required(),
-    status: Joi.string().required(),
     capacity: Joi.number().integer().required(),
     category: Joi.string().required(),
 });
